4f6758a90473e6eb4962e57f40a33a31938ac880
kamikazekartik <sreenivasan.kartik@gmail.com>
2017-10-04 23:57:09 +0530

    completed reward computation and action space
    I've built a framework that allows us to take action (by setting
    duration of the next phase) and compute the subsequent reward. Almost the entire
    RL pipeline!

--- Sumo Stuff/environment_state.py
+++ Sumo Stuff/environment_state.py
@@ -139,41 +139,62 @@ def get_down_up_dtse(y_min, y_max, x):
             block = int((y_max - y_pos) / CAR_WIDTH)
             # print x_pos, y_pos, block
             dtse_map[block] = [1, vehicle_vel, vehicle_id]
+
     return dtse_map
 
+def normalize_dtse(dtse):
+    max_vel = 0
+    for (vehicle_present, vehicle_vel, vehicle_id) in dtse:
+        max_vel = max(max_vel, vehicle_vel)
+    # avoid divide by zero
+    if max_vel == 0:
+        max_vel = 1
+    normalized_dtse = [[vehicle_present, (vehicle_vel/max_vel), vehicle_id] for (vehicle_present, vehicle_vel, vehicle_id) in dtse]
+    return normalized_dtse
 
 def get_dtse_for_junction():
+    # NOTE: all outgoing lanes have been commented because DTSE
+    # should be calculated only for incoming lanes
+
     # left-right-1
     [(x_min, y), (x_max, y1)] = traci.lane.getShape('left-right-1_0')
     lr_1_dtse = get_left_right_dtse(x_min, x_max, y)
 
-    # left-right-2  # block size will be wrong near the junction
-    [(x_min, y), (x_max, y1)] = traci.lane.getShape('left-right-2_0')
-    lr_2_dtse = get_left_right_dtse(x_min, x_max, y)
+    # # left-right-2  # block size will be wrong near the junction
+    # [(x_min, y), (x_max, y1)] = traci.lane.getShape('left-right-2_0')
+    # lr_2_dtse = get_left_right_dtse(x_min, x_max, y)
 
     # right-left-1
     [(x_max, y), (x_min, y1)] = traci.lane.getShape('right-left-1_0')
     rl_1_dtse = get_left_right_dtse(x_min, x_max, y)
 
-    # right-left-2  # block size will be wrong near the junction
-    [(x_max, y), (x_min, y1)] = traci.lane.getShape('right-left-2_0')
-    rl_2_dtse = get_left_right_dtse(x_min, x_max, y)
+    # # right-left-2  # block size will be wrong near the junction
+    # [(x_max, y), (x_min, y1)] = traci.lane.getShape('right-left-2_0')
+    # rl_2_dtse = get_left_right_dtse(x_min, x_max, y)
 
     # up-down-1
     [(x, y_max), (x1, y_min)] = traci.lane.getShape('up-down-1_0')
     ud_1_dtse = get_up_down_dtse(y_min, y_max, x)
 
-    # up-down-2  # block size will be wrong near the junction
-    [(x, y_max), (x1, y_min)] = traci.lane.getShape('up-down-2_0')
-    ud_2_dtse = get_up_down_dtse(y_min, y_max, x)
+    # # up-down-2  # block size will be wrong near the junction
+    # [(x, y_max), (x1, y_min)] = traci.lane.getShape('up-down-2_0')
+    # ud_2_dtse = get_up_down_dtse(y_min, y_max, x)
 
     # down-up-1
     [(x, y_min), (x1, y_max)] = traci.lane.getShape('down-up-1_0')
     du_1_dtse = get_down_up_dtse(y_min, y_max, x)
 
-    # down-up-2  # block size will be wrong near the junction
-    [(x, y_min), (x1, y_max)] = traci.lane.getShape('down-up-2_0')
-    du_2_dtse = get_down_up_dtse(y_min, y_max, x)
+    # # down-up-2  # block size will be wrong near the junction
+    # [(x, y_min), (x1, y_max)] = traci.lane.getShape('down-up-2_0')
+    # du_2_dtse = get_down_up_dtse(y_min, y_max, x)
+
+    dtse_list = [lr_1_dtse, rl_1_dtse, ud_1_dtse, du_1_dtse]
+    normalized_dtse_list = []
+
+    for dtse in dtse_list:
+        normalized_dtse_list.append(normalize_dtse(dtse))
+
+    return normalized_dtse_list
 
 vehicle_wait_times = defaultdict(lambda: defaultdict(lambda: 0.0))
 min_speed = 0.1
@@ -193,7 +214,7 @@ def get_avg_waiting_time_v1():
 
         # makes sure we don't include avg wait time from vehicles no longer on the network
         avg_wait_time +=  vehicle_wait_times[vehicle_id]['avg_wait_time']
-    avg_wait_time = avg_wait_time / len(vehicle_ids)
+    avg_wait_time = avg_wait_time / len(vehicle_ids) if len(vehicle_ids) > 0 else 0
 
     return avg_wait_time
 
@@ -202,14 +223,62 @@ total_moving_time = 0
 # call this at every step
 # TODO: change it to a weighted average
 # total_moving_time = (gamma * total_moving_time) + total_moving_time
-def get_avg_waiting_time():
+def get_avg_waiting_time(gamma = 1.0):
     global total_moving_time, total_waiting_time
     vehicle_ids = traci.vehicle.getIDList()
     for vehicle_id in vehicle_ids:
         if traci.vehicle.getSpeed(vehicle_id) < 0.1:
-            total_waiting_time = total_waiting_time + 1
+            total_waiting_time = gamma * total_waiting_time + 1
         else:
-            total_moving_time = total_moving_time + 1
+            total_moving_time = gamma * total_moving_time + 1
 
-    avg_wait_time = total_waiting_time / (total_waiting_time + total_moving_time)
+    avg_wait_time = total_waiting_time / (total_waiting_time + total_moving_time) if len(vehicle_ids) > 0 else 0
     return avg_wait_time
+
+# define the agent's action
+# pass the green duration for the next phase
+# NOTE: make sure to call this only at the beginning of the phase or else
+#       you may end up resetting the existing phase duration
+# default green duration is 31s
+def act(green_duration):
+    tls = traci.trafficlights.getIDList()[0]
+    curr_phase = traci.trafficlights.getPhase(tls)
+    # we can change the duration only at the beginning of phases 0, 3, 6, 9
+    if curr_phase % 3 == 0:
+        print "setting phase to {}".format(green_duration)
+        traci.trafficlights.setPhaseDuration(tls, green_duration)
+
+
+avg_wait_frac_list = []
+def test_workflow():
+    step = 0
+    while step < 1000:
+        # this represents the avg_waiting_frac the last time we took an action
+        prev_avg_waiting_frac = 0
+        prev_avg_waiting_frac = run_sim_step(step, prev_avg_waiting_frac)
+        step += 1
+
+def run_sim_step(step, prev_avg_waiting_frac):
+    tls = traci.trafficlights.getIDList()[0]
+    prev_phase = traci.trafficlights.getPhase(tls)
+    avg_wait_frac = get_avg_waiting_time()
+    avg_wait_frac_list.append(avg_wait_frac)
+    
+    traci.simulationStep()
+    curr_phase = traci.trafficlights.getPhase(tls)
+
+    if (curr_phase != prev_phase) and (curr_phase % 3 == 0):
+        # phase has changed and the agent needs to do something!
+        # get DTSE
+        dtse = get_dtse_for_junction()
+
+        # compute reward
+        # if it has reduced, we get a postive reward!
+        reward = prev_avg_waiting_frac - avg_wait_frac
+        print "reward!", reward
+        prev_avg_waiting_frac = avg_wait_frac
+
+        # act!
+        act(20)
+
+    return prev_avg_waiting_frac
